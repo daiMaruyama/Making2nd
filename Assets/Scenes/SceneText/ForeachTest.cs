87072c3ee375d81a3ac96a3c4a0b1d037472e6ce
mkgi16cd <mkgi16cd@gmail.com>
2025-09-25 21:02:56 +0900

    summary追加

--- Assets/Scenes/SceneText/ForeachTest.cs
+++ Assets/Scenes/SceneText/ForeachTest.cs
@@ -1,3 +1,9 @@
+/// <summary>
+/// TMP_Text に最初から設定されている文字列をタイプライター風に表示する処理。
+/// - foreach で文字を一文字ずつ追加し、await Task.Delay で文字送り間隔を制御
+/// - maxVisibleCharacters は使用せず、_text.text に += で文字を追加
+/// </summary>
+
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
